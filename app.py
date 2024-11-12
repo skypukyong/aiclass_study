f99f4e6ddf1f3838d13bc63859132790ab9e4bfb
haneul <didani1234@naver.com>
2024-11-12 15:20:43 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 import streamlit as st
 from openai import OpenAI
 st.write("Hello World!")
+api_key = st.text_input("apikey입력")
 prompt = st.text_input("질문을 입력하세요")
 
-client = OpenAI(api_key="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")
+client = OpenAI(api_key)
 response = client.images.generate(
   model="dall-e-3",
   prompt=prompt)
