f5bdcd963b37dd8d4b1ee15cdec20a0d8a385168
haneul <didani1234@naver.com>
2024-11-12 15:09:36 +0900

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from openai import OpenAI
 st.write("Hello World!")
 question = st.text_input("질문을 입력하세요")
 
-client = OpenAI(api_key="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")
+client = OpenAI(api_key="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")
 response = client.chat.completions.create(
   model="gpt-4o-mini",
   messages=[
