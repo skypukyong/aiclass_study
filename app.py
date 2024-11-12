bd57b66d935de8e70a438cea23e31e87d9454e39
haneul <didani1234@naver.com>
2024-11-12 15:14:12 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,11 @@
 import streamlit as st
 from openai import OpenAI
 st.write("Hello World!")
-question = st.text_input("질문을 입력하세요")
+prompt = st.text_input("질문을 입력하세요")
 
 client = OpenAI(api_key="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")
-response = client.chat.completions.create(
-  model="gpt-4o-mini",
-  messages=[
-    {"role": "user", "content": question}
-  ]
-)
-st.write(response.choices[0].message.content)
+response = client.images.generate(
+  model="dall-e-3",
+  prompt=prompt)
+image_url = response.data[0].url
+st.image(image_url)
