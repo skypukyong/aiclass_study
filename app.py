03ab4369778edc75a517cb0b388d4806ae57b4d1
haneul <didani1234@naver.com>
2024-11-12 15:16:13 +0900

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from openai import OpenAI
 st.write("Hello World!")
 prompt = st.text_input("질문을 입력하세요")
 
-client = OpenAI(api_key="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")
+client = OpenAI(api_key="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")
 response = client.images.generate(
   model="dall-e-3",
   prompt=prompt)
