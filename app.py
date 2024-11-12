1dca7ab108f92c4f6f663805fc4a53cc534afbb0
haneul <didani1234@naver.com>
2024-11-12 15:00:05 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from openai import OpenAI
 st.write("Hello World!")
-question = st.text_input("질문을 입력하세요", type="password")
+question = st.text_input("질문을 입력하세요")
 
 client = OpenAI(api_key="sk-0qdihuZM4RN-gGz7tk9HqNR6GO10poNv-oftP2HUlRT3BlbkFJnNwX5YP9slEvfPkPJzPchl9tc7oVfMN4eSsQIaw2cA")
 response = client.chat.completions.create(
