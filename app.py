731672859f946da48b5109e6430134ac22906065
haneul <didani1234@naver.com>
2024-11-13 14:38:44 +0900

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,9 @@ import streamlit as st
 from openai import OpenAI
 st.write("Hello World!")
 api_key = st.text_input("apikey입력")
- 
+if 'key' not in st.session_state:
+ st.session_state['key'] = api_key
+st.write(st.session_state['key'])
 prompt = st.text_input("질문을 입력하세요")
 client = OpenAI(api_key=api_key)
 response = client.chat.completions.create(
