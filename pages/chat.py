84aacd24bff5b48ed30229e35761d4676e483572
haneul <didani1234@naver.com>
2024-11-19 14:28:27 +0900

    Update chat.py

--- pages/chat.py
+++ pages/chat.py
@@ -11,7 +11,7 @@ if 'key' not in st.session_state:
 api_key = st.session_state.get('key')
 if api_key:
     # 질문 입력 받기
-    prompt = st.text_input("질문을 입력하세요")
+    prompt = st.chat_input("질문을 입력하세요")
 
     # OpenAI API 호출
     if prompt:  # 질문이 입력된 경우에만 API 요청
@@ -25,4 +25,4 @@ if api_key:
         )
 
         # 응답 출력
-        st.write(response.choices[0].message.content)
+        st.chat_message(response.choices[0].message.content)
