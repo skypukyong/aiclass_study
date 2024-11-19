7283551135441d49746795ccbaacc87b3e807abf
haneul <didani1234@naver.com>
2024-11-19 15:15:10 +0900

    Update app1.py

--- app1.py
+++ app1.py
@@ -1,28 +1,39 @@
 import streamlit as st
 from openai import OpenAI
-@st.cache_data
-def api_key_input_cash(api_key):
-    return api_key
-st.write("Hello World!")
-if 'key' not in st.session_state:
-    api_key = st.text_input("apikey입력")
-    if api_key:
-        st.session_state.key = api_key_input_cash(api_key)
-api_key = st.session_state.get('key')
-if api_key:
-    # 질문 입력 받기
-    prompt = st.text_input("질문을 입력하세요")
 
-    # OpenAI API 호출
-    if prompt:  # 질문이 입력된 경우에만 API 요청
-        client = OpenAI(api_key=api_key)
+api_key = st.text_input("OpenAI API Key", type='password')
+client = OpenAI(api_key=api_key)
 
-        # GPT 모델에 질문 요청
-        response = client.chat.completions.create(
-         model="gpt-4o-mini",
-         messages=[{"role": "user", "content": prompt}
-         ]
-        )
+prompt = st.text_area("Prompt")
+messages = [
+    {"role": "user", "content": prompt}
+]
 
-        # 응답 출력
-        st.write(response.choices[0].message.content)
+answer = ''
+
+if st.button("Generate"):
+
+    response = client.chat.completions.create(
+        model = "gpt-4o-mini",
+        messages = messages
+    )
+    answer = response.choices[0].message.content
+
+st.text(answer)
+
+
+image_url=''
+
+if st.button("Image"):
+    
+    response = client.images.generate(
+        model="dall-e-3",
+        prompt=prompt,
+        n=1,
+        size="1024x1024"
+    )
+    image_url = response.data[0].url
+
+if image_url:
+    st.markdown(f"![{prompt}]({image_url})")
+    
